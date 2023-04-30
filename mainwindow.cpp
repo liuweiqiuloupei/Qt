800fc7a507770273be5157d424d2b62aaa0e036a
lwq <lwq@email.com>
2023-04-30 10:17:33 +0800

    //1. 第三次提交

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QString>
+#include <QDebug>
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -8,6 +10,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug() << "第二次提交" << Qt::endl; // 第二次提交
+    qDebug() << "第三次提交" << Qt::endl; // 第三次提交
 }
 
 MainWindow::~MainWindow()
